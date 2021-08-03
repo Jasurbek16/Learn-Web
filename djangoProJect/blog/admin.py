ed2fc994d7e8727a647ea7a0ace841f49030b592
Jasurbek Mamurov <jascinmamurov7@gmail.com>
2021-08-03 23:47:31 +0500

    delete olds

--- djangoProJect/blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-# registering with the admin site
-admin.site.register(Post)
--- djangoProJect/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='blog-home'),
-    # the view to handle the logic at as 2nd arg
-    path('about/', views.about, name='blog-about'),
-]
