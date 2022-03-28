40fc421f5afa606ed8703afc15001a4d589b964e
MJey16 <jascinmamurov7@gmail.com>
2022-03-28 16:00:24 +0500

    modify database

--- Django/MySite/Projects/admin.py
+++ Django/MySite/Projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Projects, Project
+from .models import Projects, Project, Review, Tag
 
 admin.site.register(Projects)
 admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Tag)
--- Django/MySite/Projects/models.py
+++ Django/MySite/Projects/models.py
@@ -4,19 +4,23 @@ from django.db import models
 
 # Table for individual projects
 class Project(models.Model):
+    author = models.CharField(max_length=100)
+    contributors = models.CharField(max_length=100, null=True, blank=True)
+    date_started = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True, blank=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
     # owner =
-    title = models.CharField(max_length=150)
-    author = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    contributors = models.CharField(max_length=100, null=True, blank=True)
     link = models.URLField(null=True, blank=True)
     source_code = models.URLField(null=True, blank=True)
+    tag = models.ManyToManyField('Tag', blank=True)
+    title = models.CharField(max_length=150)
     # featured_image =
     vote_total = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
-    date_started = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
 # Table for all projects
@@ -24,3 +28,37 @@ class Projects(models.Model):
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=100)
+
+# Table for reviews
+
+
+class Review(models.Model):
+
+    VOTE_TYPE = (
+        ('up', 'up'),
+        ('down', 'down'),
+    )
+
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+# Table for tags
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
